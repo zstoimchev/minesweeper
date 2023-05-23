07d23a5109ed315b574a0e8bc4a064beffd2a552
zstoimchev <zivkostoimcev1551@outlook.com>
2023-05-23 18:20:57 +0200

    security check

---
+++ assets/js/audio.js
@@ -0,0 +1,8 @@
+let audioEnd = new Audio();
+const audio = new Audio('../assets/audio/tic-tac-timer.wav');
+audio.loop = true;
+
+function toggleAudioOnOff(volume) {
+    localStorage.setItem('myValue', volume);
+    console.log(volume);
+}
